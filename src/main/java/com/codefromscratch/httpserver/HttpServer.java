5aaaf8141b2154903dd249c5518b36e5e313e0b5
Paul Costanza <paulvincentcostanza@gmail.com>
2025-11-08 15:15:50 -0500

    Initial commit to project

---
+++ src/main/java/com/codefromscratch/httpserver/HttpServer.java
@@ -0,0 +1,10 @@
+package com.codefromscratch.httpserver;
+
+/**
+ * Driver Class for the Http Server
+ */
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Yo the server is starting!");
+    }
+}
